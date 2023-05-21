fdca1c39ad24e474321aea29c11fc6c70c5b01b3
mdnriyas17 <mdnriyas@gmail.com>
2023-05-21 07:11:27 +0530

    new

---
+++ notnow/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Summa from './pages/Summa';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Summa/>
+    </div>
+  );
+}
+
+export default App;
---
+++ notnow/src/pages/Summa.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./summa.css"
+const Summa = () => {
+  return (
+    <div><h1>hey guys welcome all!</h1>
+    <h3>udgcshguoguswuhweuhweauhh</h3>
+    <p>gweduiehwuhuhudhew</p></div>
+  )
+}
+
+export default Summa
